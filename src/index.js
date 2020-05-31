cc00017ba23204013c0db7246d287fe58b22e37a
Brandon Casamichana <brandoncasawork8@gmail.com>
2020-05-30 23:36:10 -0400

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from "firebase";
 import firebaseConfig from './firebase';
-import {createWorker} from 'tesseract.js';
+import { createWorker } from 'tesseract.js';
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -14,7 +14,7 @@ firebase.analytics();
 
 ReactDOM.render(
     <React.StrictMode>
-      <App/>
+        <App/>
     </React.StrictMode>,
     document.getElementById('root')
 );
@@ -36,87 +36,69 @@ const request = {sources: ['screen']};
 let stream;
 
 chrome.runtime.sendMessage(EXTENSION_ID, request, response => {
-  if (response && response.type === 'success') {
-    navigator.mediaDevices.getUserMedia({
-      video: {
-        mandatory: {
-          chromeMediaSource: 'desktop',
-          chromeMediaSourceId: response.streamId,
-        }
-      }
-    }).then(returnedStream => {
-      stream = returnedStream;
-      video.srcObject = stream;
-      video.addEventListener('canplay', function (ev) {
-        height = video.videoHeight / (video.videoWidth / width);
+    if (response && response.type === 'success') {
+        navigator.mediaDevices.getUserMedia({
+            video: {
+                mandatory: {
+                    chromeMediaSource: 'desktop',
+                    chromeMediaSourceId: response.streamId,
+                }
+            }
+        }).then(returnedStream => {
+            stream = returnedStream;
+            video.srcObject = stream;
+            video.addEventListener('canplay', function (ev) {
+                height = video.videoHeight / (video.videoWidth / width);
 
-        video.setAttribute('width', width);
-        video.setAttribute('height', height);
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
-        streaming = true;
+                video.setAttribute('width', width);
+                video.setAttribute('height', height);
+                canvas.setAttribute('width', width);
+                canvas.setAttribute('height', height);
+                streaming = true;
 
-        const cropCanvas = (sourceCanvas, left, top, width, height) => {
-          let destCanvas = document.createElement('canvas');
-          destCanvas.width = width;
-          destCanvas.height = height;
-          destCanvas.getContext("2d").drawImage(
-              sourceCanvas,
-              left, top, width, height,  // source rect with content to crop
-              0, 0, width, height);      // newCanvas, same size as source rect
-          return destCanvas;
-        }
+                const cropCanvas = (sourceCanvas,left,top,width,height) => {
+                    let destCanvas = document.createElement('canvas');
+                    destCanvas.width = width;
+                    destCanvas.height = height;
+                    destCanvas.getContext("2d").drawImage(
+                        sourceCanvas,
+                        left,top,width,height,  // source rect with content to crop
+                        0,0,width,height);      // newCanvas, same size as source rect
+                    return destCanvas;
+                }
 
-        const context = canvas.getContext('2d');
-        if (width && height) {
-          canvas.width = width;
-          canvas.height = height;
-          context.drawImage(video, 0, 0, width, height);
+                const context = canvas.getContext('2d');
+                if (width && height) {
+                    canvas.width = width;
+                    canvas.height = height;
+                    context.drawImage(video, 0, 0, width, height);
 
-          const worker = createWorker();
+                    const worker = createWorker({
+                        logger: m => console.log(m)
+                    });
 
-          (async () => {
-            await worker.load();
-            await worker.loadLanguage('eng');
-            await worker.initialize('eng');
-            const {data: {text}} = await worker.recognize(cropCanvas(canvas, width * 0.344921875, height * 0.51597222222, width * 0.02890625, height * 0.03055555555).toDataURL());
-            console.log("tank" + text);
-            await worker.terminate();
-          })();
+                    console.log(cropCanvas(canvas, 0, 0, 200, 200).toDataURL());
 
-          const worker2 = createWorker();
+                    (async () => {
+                        await worker.load();
+                        await worker.loadLanguage('eng');
+                        await worker.initialize('eng');
+                        const { data: { text } } = await worker.recognize(cropCanvas(canvas, 0, 0, 200, 200).toDataURL());
+                        console.log(text);
+                        await worker.terminate();
+                    })();
+                } else {
+                    const context = canvas.getContext('2d');
+                    context.fillStyle = "#AAA";
+                    context.fillRect(0, 0, canvas.width, canvas.height);
 
-          (async () => {
-            await worker2.load();
-            await worker2.loadLanguage('eng');
-            await worker2.initialize('eng');
-            const {data: {text}} = await worker2.recognize(cropCanvas(canvas, width * 0.49140625, height * 0.51597222222, width * 0.02890625, height * 0.03055555555).toDataURL());
-            console.log("damage" + text);
-            await worker2.terminate();
-          })();
-
-          const worker3 = createWorker();
-
-          (async () => {
-            await worker3.load();
-            await worker3.loadLanguage('eng');
-            await worker3.initialize('eng');
-            const {data: {text}} = await worker3.recognize(cropCanvas(canvas, width * 0.637109375, height * 0.51597222222, width * 0.02890625, height * 0.03055555555).toDataURL());
-            console.log("support" + text);
-            await worker3.terminate();
-          })();
-        } else {
-          const context = canvas.getContext('2d');
-          context.fillStyle = "#AAA";
-          context.fillRect(0, 0, canvas.width, canvas.height);
-
-          const data = canvas.toDataURL();
-        }
-      }, false);
-    }).catch(err => {
-      console.error('Could not get stream: ', err);
-    });
-  } else {
-    console.error('Could not get stream');
-  }
+                    const data = canvas.toDataURL();
+                }
+            }, false);
+        }).catch(err => {
+            console.error('Could not get stream: ', err);
+        });
+    } else {
+        console.error('Could not get stream');
+    }
 });
